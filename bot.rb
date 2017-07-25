90480468232d2da9cb0603cc4e039f0c973e1e92
parth modi <parthmodi54@yahoo.com>
2017-07-25 16:00:34 +0530

    shows added

--- bot.rb
+++ bot.rb
@@ -2,6 +2,7 @@ require 'facebook/messenger'
 require 'httparty' # you should require this one
 require 'feedjira'
 require 'json' # and that one
+require_relative 'shows'
 # require 'dotenv'
 # Dotenv.load
 
@@ -18,9 +19,15 @@ def wait_for_user_input
       when 'hi', 'hello', 'hey' # we use regexp to match parts of strings
         message.reply(text: 'Hey there!')
       when 'got', 'game of thrones'
-        xml = HTTParty.get("http://showrss.info/show/262.rss").body
-        feed = Feedjira::Feed.parse(xml)
-        message.reply(text: "TITLE: #{feed.entries.first.title}\n URL: #{feed.entries.first.url}")
+        show_url = Shows.search_from_shows('Yukon')
+        if show_url.nil?
+          text = 'Show Not Found!'
+          message.reply(text: text)
+        else
+          xml = HTTParty.get(show_url).body
+          feed = Feedjira::Feed.parse(xml)
+          message.reply(text: "TITLE: #{feed.entries.first.title}\n URL: #{feed.entries.first.url}")
+        end
       else
         message.reply(text: 'I know nothing.')
       end
---
+++ shows.rb
@@ -0,0 +1,39 @@
+require 'mechanize'
+require 'pry'
+class Shows
+  attr_reader :shows, :agent
+
+  def self.shows
+    @shows ||= get_shows
+  end
+
+  def self.get_shows
+    page = agent.get('http://showrss.info/browse')
+    form = page.forms.first
+    select_field = form.field_with(id: 'showselector')
+    opts = select_field.options.map do |option|
+      {
+          text: option.text,
+          url: "http://showrss.info/show/#{option.value}.rss"
+      }
+    end
+
+    Hash[opts.map { |h| h.values_at(:text, :url) }]
+  end
+
+  def self.search_from_shows(term)
+    shows.keys.each do |key|
+      puts key.downcase
+      puts term.downcase
+      if key.downcase.include?(term.downcase)
+        return shows[key]
+      else
+        nil
+      end
+    end
+  end
+
+  def self.agent
+    @agent ||= Mechanize.new
+  end
+end
