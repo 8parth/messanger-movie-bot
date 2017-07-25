d87a8b41c1c8496e529f95c4cb9edb4d5cc4f128
Parth Modi <parthmodi54@yahoo.com>
2017-07-25 09:30:28 +0530

    got info added

--- bot.rb
+++ bot.rb
@@ -1,5 +1,6 @@
 require 'facebook/messenger'
 require 'httparty' # you should require this one
+require 'feedjira'
 require 'json' # and that one
 # require 'dotenv'
 # Dotenv.load
@@ -12,14 +13,14 @@ Facebook::Messenger::Subscriptions.subscribe(access_token: ENV['ACCESS_TOKEN'])
 def wait_for_user_input
   Bot.on :message do |message|
     begin
-      puts ENV['ACCESS_TOKEN']
-      puts message.text
       message.typing_on
       case message.text.downcase
       when 'hi', 'hello', 'hey' # we use regexp to match parts of strings
         message.reply(text: 'Hey there!')
       when 'got', 'game of thrones'
-
+        xml = HTTParty.get("http://showrss.info/show/262.rss").body
+        feed = Feedjira::Feed.parse(xml)
+        message.reply(text: "TITLE: #{feed.entries.first.title}\n URL: #{feed.entries.first.url}")
       else
         message.reply(text: 'I know nothing.')
       end
